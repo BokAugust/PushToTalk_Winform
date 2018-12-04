04f4c5015a505420495aa5f731eb6dbbb94bca89
bokaugust <bokaugust@gmail.com>
2018-12-04 20:35:30 +0800

    First Upload.

---
+++ PushToTalk_Winform/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace PushToTalk_Winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.changeHotkeyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.hotKey_label = new System.Windows.Forms.Label();
+            this.changeHotkey_btn = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // changeHotkeyToolStripMenuItem
+            // 
+            this.changeHotkeyToolStripMenuItem.Name = "changeHotkeyToolStripMenuItem";
+            this.changeHotkeyToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.changeHotkeyToolStripMenuItem.Text = "Change Hotkey";
+            this.changeHotkeyToolStripMenuItem.Click += new System.EventHandler(this.changeHotkeyToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.changeHotkeyToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(157, 48);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Text = "PushToTalk";
+            this.notifyIcon1.Visible = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "The hotkey right now is :";
+            // 
+            // hotKey_label
+            // 
+            this.hotKey_label.AutoSize = true;
+            this.hotKey_label.Location = new System.Drawing.Point(262, 114);
+            this.hotKey_label.Name = "hotKey_label";
+            this.hotKey_label.Size = new System.Drawing.Size(39, 13);
+            this.hotKey_label.TabIndex = 2;
+            this.hotKey_label.Text = "Loding";
+            // 
+            // changeHotkey_btn
+            // 
+            this.changeHotkey_btn.Location = new System.Drawing.Point(136, 140);
+            this.changeHotkey_btn.Name = "changeHotkey_btn";
+            this.changeHotkey_btn.Size = new System.Drawing.Size(165, 23);
+            this.changeHotkey_btn.TabIndex = 3;
+            this.changeHotkey_btn.Text = "Change Hotkey";
+            this.changeHotkey_btn.UseVisualStyleBackColor = true;
+            this.changeHotkey_btn.Click += new System.EventHandler(this.changeHotkey_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 261);
+            this.Controls.Add(this.changeHotkey_btn);
+            this.Controls.Add(this.hotKey_label);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Opacity = 0D;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripMenuItem changeHotkeyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label hotKey_label;
+        private System.Windows.Forms.Button changeHotkey_btn;
+    }
+}
+
---
+++ PushToTalk_Winform/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
+using NAudio.CoreAudioApi;
+
+namespace PushToTalk_Winform
+{
+    public partial class Form1 : Form
+    {
+
+        private IKeyboardMouseEvents m_GlobalHook;
+        public MouseButtons unmuteDefault = MouseButtons.XButton1;
+        public MouseButtons toggleDefault = MouseButtons.XButton2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Subscribe();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.notifyIcon1.Icon = Properties.Resources.mic_off;
+            this.BeginInvoke(new Action(() => {
+                this.Hide();
+                this.Opacity = 1;
+            }));            
+        }
+        
+        public void muteMic()
+        {
+            this.notifyIcon1.Icon = Properties.Resources.mic_off;
+            setMicMuteStatus(true);
+        }
+
+        public void unmuteMic()
+        {
+            this.notifyIcon1.Icon = Properties.Resources.mic_on;
+            setMicMuteStatus(false);
+        }
+                     
+        public void setMicMuteStatus(bool doMute)
+        {
+            var device = getPrimaryMicDevice();
+
+            if (device != null)
+            {
+                device.AudioEndpointVolume.Mute = doMute;
+                updateMicStatus(device);
+            }
+            else
+            {
+                updateMicStatus(null);
+            }
+        }
+
+
+        //Function
+
+        private void updateMicStatus(MMDevice device)
+        {
+            disposeDevice(device);
+        }
+
+        private void disposeDevice(MMDevice device)
+        {
+            if (device != null)
+            {
+                device.AudioEndpointVolume.Dispose();
+                device.Dispose();
+            }
+        }
+        private MMDevice getPrimaryMicDevice()
+        {
+            var enumerator = new MMDeviceEnumerator();
+            var result = enumerator.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Communications);
+            enumerator.Dispose();
+
+            //tbIcon.Text = result.DeviceFriendlyName;
+
+            return result;
+        }
+
+        //HOTKEY
+        public void Subscribe()
+        {
+            m_GlobalHook = Hook.GlobalEvents();
+
+            m_GlobalHook.MouseDownExt += GlobalHookMouseDownExt;
+            m_GlobalHook.MouseUpExt += GlobalHookMouseUpExt;
+            m_GlobalHook.KeyPress += GlobalHookKeyPress;
+        }
+
+        private void GlobalHookKeyPress(object sender, KeyPressEventArgs e)
+        {
+            Console.WriteLine("KeyPress: \t{0}", e.KeyChar);
+        }
+
+        private void GlobalHookMouseDownExt(object sender, MouseEventExtArgs e)
+        {
+            Console.WriteLine("MouseDown: \t{0}; \t System Timestamp: \t{1}", e.Button, e.Timestamp);        
+
+            if (e.Button == toggleDefault)
+            {
+                unmuteMic();
+                e.Handled = true;
+                hotKey_label.Text = toggleDefault.ToString();
+            }
+
+            if (e.Button == unmuteDefault)
+            {
+                unmuteMic();
+                e.Handled = true;
+            }
+
+        }
+
+        private void GlobalHookMouseUpExt(object sender, MouseEventExtArgs e)
+        {
+            Console.WriteLine("MouseDown: \t{0}; \t System Timestamp: \t{1}", e.Button, e.Timestamp);
+
+            if (e.Button == toggleDefault)
+            {
+                muteMic();
+                e.Handled = true;
+                hotKey_label.Text = toggleDefault.ToString();
+            }
+
+        }
+
+        public void Unsubscribe()
+        {
+            m_GlobalHook.MouseDownExt -= GlobalHookMouseDownExt;
+            m_GlobalHook.KeyPress -= GlobalHookKeyPress;
+
+            //It is recommened to dispose it 
+            m_GlobalHook.Dispose();
+        }
+
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            notifyIcon1.Dispose();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            notifyIcon1.Dispose();
+        }
+
+        private void changeHotkeyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Opacity = 100;
+            this.Show();
+        }
+
+        private void changeHotkey_btn_Click(object sender, EventArgs e)
+        {
+            //hotKey_label.Text = "Waiting...";
+            //setKeys = true;
+            //_changeHotkey();
+
+            //this.Enabled = false;
+        }
+
+    }
+}
