139c33125f0abc5077353382a6979e72b370ea26
bokaugust <bokaugust@gmail.com>
2018-12-12 18:12:16 +0800

    fix some bugs.

--- PushToTalk_Winform/Properties/AssemblyInfo.cs
+++ PushToTalk_Winform/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("PushToTalk")]
-[assembly: AssemblyDescription("A simple tool gives you a choice for Push To Talk.")]
+[assembly: AssemblyDescription("A simple tool gives you a choice for PushToTalk.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PushToTalk")]
-[assembly: AssemblyCopyright("Copyright August ©  2018")]
+[assembly: AssemblyCopyright("Copyright August © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
-// [assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
